fd90fe988f2bf20e19f9b8cd81a8e0c893fbb76e
antoinepetty <36314396+antoinepetty@users.noreply.github.com>
2020-05-08 17:40:07 +0100

    tidy example controller

--- controllers/example_controller/example_controller.py
+++ controllers/example_controller/example_controller.py
@@ -1,4 +1,3 @@
-#from controller import  Motor, Camera, Robot as WebotsRobot
 import time
 
 from sr.robot import *
@@ -6,14 +5,10 @@ from sr.robot import *
 R = Robot()
 
 while True:
-
     R.motors[0].m0.power = float(5.0)
-    #R.motors[0].m1.power = float(20.0)
     R.motors[0].m1.power = float(-5.0)
-    #robot.getMotor("wheel1").setVelocity(1.0)
     time.sleep(0.5)
     R.motors[0].m0.power = float(0.0)
-    #R.motors[0].m1.power = float(0.0)
     R.motors[0].m1.power = float(0.0)
     time.sleep(0.5)
     
