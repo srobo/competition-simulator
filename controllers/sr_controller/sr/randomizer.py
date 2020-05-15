6cafefcd6fa8916ca3c04235aea1039f700be133
antoinepetty <36314396+antoinepetty@users.noreply.github.com>
2020-05-15 20:41:30 +0100

    Add randomizer class

---
+++ controllers/sr_controller/sr/randomizer.py
@@ -0,0 +1,17 @@
+DEFAULT_RANDOM_RANGE_PERCENT = 5 # The maximum randomness which can be added in either direction
+
+def add_jitter(actual_value, min, max, random_range_percent = DEFAULT_RANDOM_RANGE):
+    full_range = float(max) - float(min)
+
+    random_range = full_range * (random_range_percent / float(100))
+    
+    random.seed(random.randint(min, max))
+
+    actual_value = actual_value + random.uniform(-random_range, random_range)
+
+    if actual_value > max:
+        actual_value = max
+    elif actual_value < min:
+        actual_value = min
+
+    return actual_value
--- controllers/sr_controller/sr/robot/motor.py
+++ controllers/sr_controller/sr/robot/motor.py
@@ -12,9 +12,6 @@ MOTOR_NAMES['lift motor'] = LINEAR
 MOTOR_NAMES['left finger motor'] = LINEAR
 MOTOR_NAMES['right finger motor'] = LINEAR
 
-
-RANDOM_RANGE = 5 # The maximum randomness which can be added in either direction
-
 def get_motor_id(board, channel):
     return list(MOTOR_NAMES.keys())[(board*2)+channel]
 
