1636b974cdf9559dd569c664b9581e60dbd51cd7
Antoine <36314396+antoinepetty@users.noreply.github.com>
2020-05-19 18:36:30 +0100

    Change == to is
    Co-authored-by: Dan Trickey <trickeydan@users.noreply.github.com>

--- controllers/sr_controller/sr/robot/motor_devices.py
+++ controllers/sr_controller/sr/robot/motor_devices.py
@@ -5,7 +5,7 @@ class MotorBase(object):
     def initialise_motor(self, motor_name):
         self.motor_name = motor_name
         self.webot_motor = self.webot.getMotor(motor_name)
-        if self.webot_motor == None:
+        if self.webot_motor is None:
             return
         self.max_speed = self.webot_motor.getMaxVelocity()
 
