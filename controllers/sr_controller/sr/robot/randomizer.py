8f144b46e5de30f38f5a2c471ea43591d89991a3
Jake Howard <git@theorangeone.net>
2020-05-23 17:29:54 +0100

    Use uniform over randint
    uniform supports floats

--- controllers/sr_controller/sr/robot/randomizer.py
+++ controllers/sr_controller/sr/robot/randomizer.py
@@ -4,5 +4,5 @@ DEFAULT_RANDOM_RANGE_PERCENT = 10 # The maximum randomness which can be added in
 
 def add_jitter(actual_value, min_possible, max_possible, random_range_percent = DEFAULT_RANDOM_RANGE_PERCENT):
     random_range = actual_value * (random_range_percent / float(100))
-    new_value = actual_value + random.randint(-random_range, random_range)
+    new_value = actual_value + random.uniform(-random_range, random_range)
     return max(min_possible, min(new_value, max_possible))
