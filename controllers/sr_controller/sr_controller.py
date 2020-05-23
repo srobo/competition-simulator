23da1295cc4396f1d0b132b63923aa22da2d5717
antoinepetty <36314396+antoinepetty@users.noreply.github.com>
2020-05-23 20:27:48 +0100

    restore example controller program

--- controllers/sr_controller/sr_controller.py
+++ controllers/sr_controller/sr_controller.py
@@ -7,3 +7,19 @@ R = Robot()
 while True:
     print("Dist L:", R.ruggeduinos[0].analogue_read(0))
     print("Dist R:", R.ruggeduinos[0].analogue_read(1))
+    R.motors[0].m0.power = 10
+    R.motors[0].m1.power = 10
+    R.motors[1].m1.power = 0
+    time.sleep(2)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.motors[1].m1.power = -100
+    time.sleep(2)
+    R.motors[0].m0.power = -10
+    R.motors[0].m1.power = -10
+    R.motors[1].m1.power = 0
+    time.sleep(2)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.motors[1].m1.power = 100
+    time.sleep(2)
