ed7ae87c61336d02ce532140fee4f74f257538fa
Jake Howard <git@theorangeone.net>
2020-05-24 21:27:19 +0100

    Add slightly prettier game over message

--- controllers/sr_controller/sr/robot/game.py
+++ controllers/sr_controller/sr/robot/game.py
@@ -2,11 +2,16 @@ import time
 import _thread
 from threading import Timer
 from sr.robot.settings import GAME_DURATION
+from textwrap import dedent
+
+def game_over():
+    print(dedent("""
+    ==========
+    Game Over!
+    ==========
+    """).strip())
+    _thread.interrupt_main()
 
 
 def stop_after_delay(delay=GAME_DURATION):
-    def game_over():
-        print("Game over!")
-        _thread.interrupt_main()
-
     Timer(delay, game_over).start()
