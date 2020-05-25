23cc98f93f3d68fbb5f74542ba7566ff07305876
Peter Law <PeterJCLaw@gmail.com>
2020-05-25 20:25:24 +0100

    Add an (untested as yet) api
    This contains high level utils for converting from raw Webots
    recognition objects to our Token objects.

---
+++ api.py
@@ -0,0 +1,50 @@
+from typing import List, Tuple, Sequence, TYPE_CHECKING
+
+from image import Rectangle
+from tokens import Token
+from convert import WebotsOrientation, rotation_matrix_from_axis_and_angle
+from vectors import Vector
+
+if TYPE_CHECKING:
+    from webots import CameraRecognitionObject
+
+
+def build_token_and_rectangle(
+    recognition_object: 'CameraRecognitionObject',
+) -> Tuple[Token, Rectangle]:
+    token = Token(position=Vector(recognition_object.get_position()))
+    token.rotate(rotation_matrix_from_axis_and_angle(
+        WebotsOrientation(*recognition_object.get_orientation()),
+    ))
+
+    return (
+        token,
+        Rectangle(
+            recognition_object.get_position_on_image(),
+            recognition_object.get_size_on_image(),
+        ),
+    )
+
+
+def tokens_from_objects(objects: 'Sequence[CameraRecognitionObject]') -> Sequence[Token]:
+    """
+    Constructs tokens from the given recognised objects, ignoring any which are
+    judged not to be visible to the camera.
+    """
+
+    # TODO: filter out non-tokens
+
+    tokens_with_image_rectangles = sorted(
+        (build_token_and_rectangle(o) for o in objects),
+        key=lambda x: x[0].position.magnitude(),
+    )
+
+    preceding_rectangles = []  # type: List[Rectangle]
+    tokens = []
+    for token, image_rectangle in tokens_with_image_rectangles:
+        if not any(x.overlaps(image_rectangle) for x in preceding_rectangles):
+            tokens.append(token)
+
+        preceding_rectangles.append(image_rectangle)
+
+    return tokens
--- example.py
+++ example.py
@@ -1,24 +1,11 @@
-from tokens import Token
+from typing import Sequence
+
+import api
 from webots import CameraRecognitionObject
-from convert import WebotsOrientation, rotation_matrix_from_axis_and_angle
-from vectors import Vector
 
-webots_camera_recognition_object = NotImplemented  # type: CameraRecognitionObject
+webots_camera_recognition_objects = NotImplemented  # type: Sequence[CameraRecognitionObject]
 
-TOKEN_SIZE = 42 # TODO?
-
-# TODO: use `webots_camera_recognition_object.get_position_on_image` and
-# `webots_camera_recognition_object.get_size_on_image` to determine whether the
-# object can actually be seen.
-
-token = Token(
-    position=Vector(webots_camera_recognition_object.get_position()),
-    size=TOKEN_SIZE,
-)
-token.rotate(rotation_matrix_from_axis_and_angle(WebotsOrientation(
-    *webots_camera_recognition_object.get_orientation(),
-)))
-
-for face in token.visible_faces():
-    orientation = face.orientation()
-    print(orientation)
+for token in api.tokens_from_objects(webots_camera_recognition_objects):
+    for face in token.visible_faces():
+        orientation = face.orientation()
+        print(orientation)
