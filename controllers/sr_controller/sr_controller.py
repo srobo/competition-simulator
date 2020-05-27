e461b7b0c54796ab122774414c49500e131856ba
Jake Howard <git@theorangeone.net>
2020-05-27 22:01:18 +0100

    Use `execvpe` to run the subprocess rather than munging the python path and importing
    Results in cleaner code which doens't do weird python things. Not sure if execvpe is cross-platform, though

--- controllers/sr_controller/sr_controller.py
+++ controllers/sr_controller/sr_controller.py
@@ -5,14 +5,9 @@ from pathlib import Path
 
 ROOT = Path(os.getcwd()).joinpath("../../").resolve()
 
-CONTROLLER_NAME = "robot"
-CONTROLLER_FILENAME = CONTROLLER_NAME + ".py"
+ROBOT_FILE = ROOT.parent.joinpath("robot.py")
 
-ROBOT_FILE_DIR = ROOT.parent
-
-ROBOT_FILE = ROBOT_FILE_DIR.joinpath(CONTROLLER_FILENAME)
-
-EXAMPLE_CONTROLLER_FILE =ROOT.joinpath("controllers/example_controller/example_controller.py").resolve()
+EXAMPLE_CONTROLLER_FILE = ROOT.joinpath("controllers/example_controller/example_controller.py")
 
 
 if __name__ == "__main__":
@@ -20,5 +15,6 @@ if __name__ == "__main__":
         print("Robot controller not found, copying example into place.")
         copyfile(EXAMPLE_CONTROLLER_FILE, ROBOT_FILE)
 
-    sys.path.insert(0, str(ROBOT_FILE_DIR))
-    __import__(CONTROLLER_NAME)
+    env = os.environ.copy()
+    env['PYTHONPATH'] = os.pathsep.join(sys.path)
+    os.execvpe(sys.executable, ["-u", str(ROBOT_FILE)], env)
