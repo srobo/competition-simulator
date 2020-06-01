fa72d8f4c209cfb6853f6269153dd9bbf671ba71
Jake Howard <git@theorangeone.net>
2020-06-01 15:43:32 +0100

    It should be at least python 3.7

--- controllers/sr_controller/sr/robot/_version_check.py
+++ controllers/sr_controller/sr/robot/_version_check.py
@@ -2,7 +2,7 @@ import sys
 
 # Provide a nicer check that the user has the right version of Python than
 # them getting a `SyntaxError`
-assert sys.version_info == (3, 7), (
-    "Sorry, you must be using Python version 3.7. "
+assert sys.version_info >= (3, 7), (
+    "Sorry, you must be using at least Python 3.7. "
     "Please see the SR docs for how to switch your Python version."
 )
