21b8c9df2eba6f01e3331781c02cecd79df1e32e
antoinepetty <36314396+antoinepetty@users.noreply.github.com>
2020-06-01 14:40:37 +0100

    Update _version_check.py

--- controllers/sr_controller/sr/robot/_version_check.py
+++ controllers/sr_controller/sr/robot/_version_check.py
@@ -2,7 +2,7 @@ import sys
 
 # Provide a nicer check that the user has the right version of Python than
 # them getting a `SyntaxError`
-assert sys.version_info >= (3, 5), (
-    "Sorry, you must be using a recent version of Python 3. "
+assert sys.version_info == (3, 7), (
+    "Sorry, you must be using Python version 3.7. "
     "Please see the SR docs for how to switch your Python version."
 )
