3b2e593a993ea2908c1204b4754edd45c31abdac
Peter Law <PeterJCLaw@gmail.com>
2020-06-13 14:56:15 +0100

    Remove robots which are not in this match

--- controllers/competition_supervisor/competition_supervisor.py
+++ controllers/competition_supervisor/competition_supervisor.py
@@ -1,7 +1,12 @@
+import sys
 from pathlib import Path
 
 # Webots specific library
 from controller import Supervisor  # isort:skip
+
+sys.path.append(str(Path(__file__).resolve().parent.parent / 'sr_controller'))
+
+sr_controller = __import__('sr_controller')
 
 TIME_STEP = 32
 GAME_DURATION_SECONDS = 10
@@ -19,6 +24,24 @@ if not MODE_FILE.exists() or MODE_FILE.read_text().strip() != 'comp':
 def prepare(supervisor: Supervisor) -> None:
     supervisor.simulationSetMode(Supervisor.SIMULATION_MODE_PAUSE)
     supervisor.simulationReset()
+
+
+def remove_unused_robots(supervisor: Supervisor) -> None:
+    for webots_id_str, zone_id in sr_controller.ROBOT_IDS_TO_CORNERS.items():
+        if sr_controller.get_zone_robot_file_path(zone_id).exists():
+            continue
+
+        # Remove the robot
+        robot = supervisor.getFromId(int(webots_id_str))
+        if robot is None:
+            msg = "Failed to get Webots node for zone {} (id: {})".format(
+                zone_id,
+                webots_id_str,
+            )
+            print(msg)
+            raise ValueError(msg)
+
+        robot.remove()
 
 
 def run_match(supervisor: Supervisor) -> None:
@@ -43,7 +66,7 @@ def main() -> None:
 
     prepare(supervisor)
 
-    # TODO: connect up to wait_start and remove robots which are not present.
+    remove_unused_robots(supervisor)
 
     run_match(supervisor)
 
