ddd3d6ba9657ff7f96270e6adf3f70a9a1acbc7d
Peter Law <PeterJCLaw@gmail.com>
2020-10-24 13:18:17 +0100

    Add another docstring

--- modules/sr/robot/utils.py
+++ modules/sr/robot/utils.py
@@ -1,2 +1,3 @@
 def map_to_range(old_min, old_max, new_min, new_max, value):
+    """Maps a value from within one range of inputs to within a range of outputs."""
     return ((value - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min
