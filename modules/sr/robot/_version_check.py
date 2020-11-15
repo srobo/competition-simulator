2d9c8523b10aec8f15c760b1789bab0a4d88aa52
Peter Law <PeterJCLaw@gmail.com>
2020-11-15 23:21:48 +0000

    Update this version check to match what we support
    Missed in cfd6928.

--- modules/sr/robot/_version_check.py
+++ modules/sr/robot/_version_check.py
@@ -2,7 +2,7 @@ import sys
 
 # Provide a nicer check that the user has the right version of Python than
 # them getting a `SyntaxError`
-assert sys.version_info >= (3, 5), (
+assert sys.version_info >= (3, 7), (
     "Sorry, you must be using a recent version of Python 3. "
     "Please see the SR docs for how to switch your Python version."
 )
