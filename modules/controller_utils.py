53bf2baf36910929dadf71acad397a08e735f440
Peter Law <PeterJCLaw@gmail.com>
2020-11-26 20:21:11 +0000

    Set REPO_ROOT to the right level
    I think this was a copy/pasta error.

--- modules/controller_utils.py
+++ modules/controller_utils.py
@@ -4,7 +4,7 @@ from typing import IO, Optional
 from pathlib import Path
 
 # Root directory of the SR webots simulator (equivalent to the root of the git repo)
-REPO_ROOT = Path(__file__).resolve().parent.parent.parent
+REPO_ROOT = Path(__file__).resolve().parent.parent
 
 # Root directory of the specification of the Arena (and match)
 ARENA_ROOT = Path(os.environ.get('ARENA_ROOT', REPO_ROOT.parent))
