ce7c043640223a4a11405c249c8498ca8a45bb0e
Will Barber <will@rudi.org.uk>
2020-12-23 17:01:07 +0000

    Don't forget the spaces

--- controllers/competition_supervisor/competition_supervisor.py
+++ controllers/competition_supervisor/competition_supervisor.py
@@ -151,13 +151,13 @@ def run_match(supervisor: Supervisor) -> None:
     else:
         # webots-2021a removed the RUN mode and now uses FAST
         print(
-            "All official competition matches will be run on"
+            "All official competition matches will be run on "
             "Webots version 2020b-rev1 unless otherwise stated",
             file=sys.stderr,
         )
         print(
-            "Since this simulator is running a different version"
-            "it is possible that some behaviour may not match"
+            "Since this simulator is running a different version "
+            "it is possible that some behaviour may not match "
             "that of the official competition matches",
             file=sys.stderr,
         )
