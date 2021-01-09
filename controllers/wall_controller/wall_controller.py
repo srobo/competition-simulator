047d70d33df0039c03bbd98aaece86a8945cc225
WillB97 <will@rudi.org.uk>
2021-01-09 17:03:48 +0000

    Add moving walls
    Currently the wall fall at 0 seconds, for dev purposes.

---
+++ controllers/wall_controller/wall_controller.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from controller import Node, Supervisor
+
+
+class WallController:
+    def __init__(self, wall_time: int) -> None:
+        self._robot = Supervisor()
+        self.wall_time = wall_time
+        self.walls: List[Node] = [
+            self._robot.getFromDef('moving_wall1'),
+            self._robot.getFromDef('moving_wall2'),
+        ]
+
+    def main(self) -> None:
+        # wait for the walls to start moving in ms
+        self._robot.step(self.wall_time * 1000)
+
+        print('Moving arena walls')  # noqa: T001
+        for wall in self.walls:
+            wall.setVelocity([0, -0.3, 0, 0, 0, 0])
+
+        # wait for the walls to reach their final location
+        self._robot.step(1000)
+
+        for wall in self.walls:
+            wall.resetPhysics()
+
+
+if __name__ == "__main__":
+    wall_controller = WallController(0)
+    wall_controller.main()
