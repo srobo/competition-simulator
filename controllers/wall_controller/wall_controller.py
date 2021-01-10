e0bbaa4d0071e5a064426c02222e446f72620e45
WillB97 <will@rudi.org.uk>
2021-01-10 18:37:11 +0000

    Remove unnecessary class from WallController

--- controllers/wall_controller/wall_controller.py
+++ controllers/wall_controller/wall_controller.py
@@ -5,31 +5,26 @@ from controller import Node, Supervisor
 # Velocity matrices contain linear and rotational velocities [x, y, z, rot_x, rot_y, rot_z]
 linear_downwards = [0, -0.3, 0, 0, 0, 0]
 
+def main(wall_time: int) -> None:
+    robot = Supervisor()
+    walls: List[Node] = [
+        robot.getFromDef('moving_wall1'),
+        robot.getFromDef('moving_wall2'),
+    ]
 
-class WallController:
-    def __init__(self, wall_time: int) -> None:
-        self._robot = Supervisor()
-        self.wall_time = wall_time
-        self.walls: List[Node] = [
-            self._robot.getFromDef('moving_wall1'),
-            self._robot.getFromDef('moving_wall2'),
-        ]
+    # wait for the walls to start moving in ms
+    robot.step(wall_time * 1000)
 
-    def main(self) -> None:
-        # wait for the walls to start moving in ms
-        self._robot.step(self.wall_time * 1000)
+    print('Moving arena walls')  # noqa: T001
+    for wall in walls:
+        wall.setVelocity(linear_downwards)
 
-        print('Moving arena walls')  # noqa: T001
-        for wall in self.walls:
-            wall.setVelocity(linear_downwards)
+    # wait for the walls to reach their final location
+    robot.step(1000)
 
-        # wait for the walls to reach their final location
-        self._robot.step(1000)
-
-        for wall in self.walls:
-            wall.resetPhysics()
+    for wall in walls:
+        wall.resetPhysics()
 
 
 if __name__ == "__main__":
-    wall_controller = WallController(0)  # TODO decide the time that the walls should move
-    wall_controller.main()
+    main(0)  # TODO decide the time that the walls should move
