66961fa7dcd01e50da9d573b6860f20af073386c
Will Barber <will@rudi.org.uk>
2021-01-15 11:54:55 +0000

    Raise an error when territory controller can't find a territory

--- controllers/territory_controller/territory_controller.py
+++ controllers/territory_controller/territory_controller.py
@@ -151,10 +151,13 @@ class TerritoryController:
 
         new_colour = ZONE_COLOURS[claimed_by]
         station = self._robot.getFromDef(station_code)
-        if station is not None:
-            station.getField("zoneColour").setSFColor(
-                list(new_colour),
+        if station is None:
+            raise RuntimeError(
+                f"Failed to fetch territory node {station_code}"
             )
+        station.getField("zoneColour").setSFColor(
+            list(new_colour),
+        )
 
         self._claim_log.log_territory_claim(station_code, claimed_by, self._robot.getTime())
 
