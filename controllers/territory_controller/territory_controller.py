adbefeda90ede6fb2829fb0d4907b7a19c91c5d4
WillB97 <will@rudi.org.uk>
2021-01-17 16:07:29 +0000

    Convert territory_links to use StationCode

--- controllers/territory_controller/territory_controller.py
+++ controllers/territory_controller/territory_controller.py
@@ -63,27 +63,27 @@ LINK_COLOURS: Dict[Claimant, Tuple[float, float, float]] = {
     Claimant.UNCLAIMED: (0.25, 0.25, 0.25),
 }
 
-TERRITORY_LINKS: Tuple[str, ...] = (
-    'PN-EY',
-    'BG-OX',
-    'OX-TS',
-    'TS-VB',
-    'EY-BE',
-    'VB-BE',
-    'VB-SZ',
-    'BE-SZ',
-    'BE-PO',
-    'SZ-SW',
-    'PO-YL',
-    'SW-BN',
-    'HV-BN',
+TERRITORY_LINKS: Tuple[Tuple[Union[StationCode, TerritoryRoot], StationCode], ...] = (
+    (StationCode.PN, StationCode.EY),  # PN-EY
+    (StationCode.BG, StationCode.OX),  # BG-OX
+    (StationCode.OX, StationCode.TS),  # OX-TS
+    (StationCode.TS, StationCode.VB),  # TS-VB
+    (StationCode.EY, StationCode.BE),  # EY-BE
+    (StationCode.VB, StationCode.BE),  # VB-BE
+    (StationCode.VB, StationCode.SZ),  # VB-SZ
+    (StationCode.BE, StationCode.SZ),  # BE-SZ
+    (StationCode.BE, StationCode.PO),  # BE-PO
+    (StationCode.SZ, StationCode.SW),  # SZ-SW
+    (StationCode.PO, StationCode.YL),  # PO-YL
+    (StationCode.SW, StationCode.BN),  # SW-BN
+    (StationCode.HV, StationCode.BN),  # HV-BN
     # These links are between territories and the starting zones
-    'z0-PN',
-    'z0-TS',
-    'z0-BG',
-    'z1-YL',
-    'z1-SW',
-    'z1-HV',
+    (TerritoryRoot.z0, StationCode.PN),  # z0-PN
+    (TerritoryRoot.z0, StationCode.TS),  # z0-TS
+    (TerritoryRoot.z0, StationCode.BG),  # z0-BG
+    (TerritoryRoot.z1, StationCode.YL),  # z1-YL
+    (TerritoryRoot.z1, StationCode.SW),  # z1-SW
+    (TerritoryRoot.z1, StationCode.HV),  # z1-HV
 )
 
 
@@ -146,16 +146,7 @@ class AttachedTerritories:
     ) -> Dict[Union[StationCode, TerritoryRoot], List[StationCode]]:
         adjacent_zones: Dict[Union[StationCode, TerritoryRoot], List[StationCode]] = {}
 
-        for link in TERRITORY_LINKS:
-            link_zones = link.split('-')
-            link_codes: Tuple[Union[StationCode, TerritoryRoot], StationCode]
-
-            try:
-                link_codes = (StationCode(link_zones[0]), StationCode(link_zones[1]))
-            except ValueError:
-                # one of the starting corners
-                link_codes = (TerritoryRoot(link_zones[0]), StationCode(link_zones[1]))
-
+        for link_codes in TERRITORY_LINKS:
             for index in range(2):
                 # links with stations at both ends are reversable
                 try:
@@ -378,32 +369,27 @@ class TerritoryController:
                 receiver.nextPacket()
 
     def update_territory_links(self) -> None:
-        for link in TERRITORY_LINKS:
-            claimed_by = Claimant.UNCLAIMED
-            if link.startswith('z'):  # starting zone connection
-                link_zones = link.split('-')
-                zone_claimant = self._claim_log.get_claimant(
-                    StationCode(link_zones[1]),
-                )
-
-                # if the zone is owned by the starting zone
-                if (int(link_zones[0][-1]) == zone_claimant):
-                    claimed_by = zone_claimant
+        for stn_a, stn_b in TERRITORY_LINKS:
+            if isinstance(stn_a, TerritoryRoot):  # strating zone is implicitly owned
+                if stn_a == TerritoryRoot.z0:
+                    stn_a_claimant = Claimant.ZONE_0
+                else:
+                    stn_a_claimant = Claimant.ZONE_1
             else:
-                link_zones = link.split('-')
-                zone_claimants = [
-                    self._claim_log.get_claimant(StationCode(zone))
-                    for zone in link_zones
-                ]
+                stn_a_claimant = self._claim_log.get_claimant(stn_a)
 
-                # if both ends are owned by the same Claimant
-                if (zone_claimants[0] == zone_claimants[1]):
-                    claimed_by = zone_claimants[0]
+            stn_b_claimant = self._claim_log.get_claimant(stn_b)
+
+            # if both ends are owned by the same Claimant
+            if stn_a_claimant == stn_b_claimant:
+                claimed_by = stn_a_claimant
+            else:
+                claimed_by = Claimant.UNCLAIMED
 
             new_colour = LINK_COLOURS[claimed_by]
-            self._robot.getFromDef(link).getField("zoneColour").setSFColor(
-                list(new_colour),
-            )
+            self._robot.getFromDef(
+                '-'.join((stn_a, stn_b)),
+            ).getField("zoneColour").setSFColor(list(new_colour))
 
     def receive_robot_captures(self) -> None:
         for station_code, receiver in self._receivers.items():
