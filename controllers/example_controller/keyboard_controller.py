299c778031f68674e7c1420c9ec5d1a63a070d13
Jenny <jenny.lynn.fletcher@gmail.com>
2021-01-17 12:25:21 +0000

    add example robot that can be controlled by keyboard

---
+++ controllers/example_controller/keyboard_controller.py
@@ -0,0 +1,34 @@
+from controller import Keyboard
+from sr.robot import *
+
+R = Robot()
+
+timestep = 16
+
+keyboard=Keyboard()
+keyboard.enable(timestep)
+
+while True:
+    key=keyboard.getKey()
+    if key == -1:
+        R.motors[0].m0.power = 0
+        R.motors[0].m1.power = 0
+    else:
+        while key != -1:
+            if (key==ord('W')):
+                R.motors[0].m0.power = 50
+                R.motors[0].m1.power = 50
+            elif (key==ord('S')):
+                R.motors[0].m0.power = -50
+                R.motors[0].m1.power = -50
+            elif (key==ord('A')):
+                R.motors[0].m0.power = -25
+                R.motors[0].m1.power = 25
+            elif (key==ord('D')):
+                R.motors[0].m0.power = 25
+                R.motors[0].m1.power = -25
+            elif (key==ord('E')):
+                R.radio.claim_territory()
+            key=keyboard.getKey()
+
+    R.sleep(timestep/1000.)
