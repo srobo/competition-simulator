18ca0202c8a7b04a47c1763f99f5e4afcd01b6e2
WillB97 <will@rudi.org.uk>
2021-01-20 23:55:23 +0000

    Whitespace linting

--- controllers/example_controller/keyboard_controller.py
+++ controllers/example_controller/keyboard_controller.py
@@ -5,31 +5,31 @@ R = Robot()
 
 timestep = 16
 
-keyboard=Keyboard()
+keyboard = Keyboard()
 keyboard.enable(timestep)
-#Need to click on 3D viewport for keyboard events to be picked up by webots
+# Need to click on 3D viewport for keyboard events to be picked up by webots
 
 while True:
-    key=keyboard.getKey()
+    key = keyboard.getKey()
     if key == -1:
         R.motors[0].m0.power = 0
         R.motors[0].m1.power = 0
     else:
         while key != -1:
-            if (key==ord('W')):
+            if (key == ord('W')):
                 R.motors[0].m0.power = 50
                 R.motors[0].m1.power = 50
-            elif (key==ord('S')):
+            elif (key == ord('S')):
                 R.motors[0].m0.power = -50
                 R.motors[0].m1.power = -50
-            elif (key==ord('A')):
+            elif (key == ord('A')):
                 R.motors[0].m0.power = -25
                 R.motors[0].m1.power = 25
-            elif (key==ord('D')):
+            elif (key == ord('D')):
                 R.motors[0].m0.power = 25
                 R.motors[0].m1.power = -25
-            elif (key==ord('E')):
+            elif (key == ord('E')):
                 R.radio.claim_territory()
-            key=keyboard.getKey()
+            key = keyboard.getKey()
 
-    R.sleep(timestep/1000.)
+    R.sleep(timestep / 1000.)
