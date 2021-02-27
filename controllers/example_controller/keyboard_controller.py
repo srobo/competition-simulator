9b32ceca64e3938241524c74bef8c404078da941
Jake Howard <git@theorangeone.net>
2021-02-27 17:36:37 +0000

    Add boost key to drive faster
    Or, in other words, https://i.kinja-img.com/gawker-media/image/upload/s--bcFWzwu2--/18n2u2jtl147mjpg.jpg

--- controllers/example_controller/keyboard_controller.py
+++ controllers/example_controller/keyboard_controller.py
@@ -5,12 +5,13 @@ KEYBOARD_SAMPLING_FREQUENCY = 16
 NO_KEY_PRESSED = -1
 
 CONTROLS = {
-    "forward": ("W", "I"),
-    "reverse": ("S", "K"),
-    "left": ("A", "J"),
-    "right": ("D", "L"),
-    "claim": ("E", "O"),
-    "sense": ("Q", "U"),
+    "forward": (ord("W"), ord("I")),
+    "reverse": (ord("S"), ord("K")),
+    "left": (ord("A"), ord("J")),
+    "right": (ord("D"), ord("L")),
+    "claim": (ord("E"), ord("O")),
+    "sense": (ord("Q"), ord("U")),
+    "boost": (Keyboard.SHIFT, Keyboard.CONTROL),
 }
 
 
@@ -37,12 +38,13 @@ R = Robot()
 keyboard = Keyboard()
 keyboard.enable(KEYBOARD_SAMPLING_FREQUENCY)
 
-key_forward = ord(CONTROLS["forward"][R.zone])
-key_reverse = ord(CONTROLS["reverse"][R.zone])
-key_left = ord(CONTROLS["left"][R.zone])
-key_right = ord(CONTROLS["right"][R.zone])
-key_claim = ord(CONTROLS["claim"][R.zone])
-key_sense = ord(CONTROLS["sense"][R.zone])
+key_forward = CONTROLS["forward"][R.zone]
+key_reverse = CONTROLS["reverse"][R.zone]
+key_left = CONTROLS["left"][R.zone]
+key_right = CONTROLS["right"][R.zone]
+key_claim = CONTROLS["claim"][R.zone]
+key_sense = CONTROLS["sense"][R.zone]
+key_boost = CONTROLS["boost"][R.zone]
 
 print(
     "Note: you need to click on 3D viewport for keyboard events to be picked "
@@ -63,7 +65,15 @@ while True:
                 R.motors[0].m0.power = 50
                 R.motors[0].m1.power = 50
 
+            elif key == key_boost + key_forward:
+                R.motors[0].m0.power = 100
+                R.motors[0].m1.power = 100
+
             elif key == key_reverse:
+                R.motors[0].m0.power = -50
+                R.motors[0].m1.power = -50
+
+            elif key == key_boost + key_reverse:
                 R.motors[0].m0.power = -50
                 R.motors[0].m1.power = -50
 
