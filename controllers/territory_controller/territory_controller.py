ab379aa9a3a79efe9d0f84a59cb01ff766089cf5
Will Barber <will@rudi.org.uk>
2021-03-06 16:09:52 +0000

    Update live scoring to support different value territories

--- controllers/territory_controller/territory_controller.py
+++ controllers/territory_controller/territory_controller.py
@@ -73,6 +73,14 @@ class TerritoryRoot(str, enum.Enum):
     z1 = 'z1'
 
 
+EXTRA_VALUE_TERRITORIES = {
+    StationCode.TH: 4,
+    StationCode.FL: 4,
+    StationCode.SF: 4,
+    StationCode.HA: 4,
+    StationCode.YT: 8,
+}
+
 # Updating? Update `Arena.wbt` too
 ZONE_COLOURS: Dict[Claimant, Tuple[float, float, float]] = {
     Claimant.ZONE_0: (1, 0, 1),
@@ -198,12 +206,16 @@ class ClaimLog:
         return self._log_is_dirty
 
     def get_scores(self) -> Mapping[Claimant, int]:
-        territory_counts = collections.Counter(self._station_statuses.values())
+        zone_to_territories = collections.defaultdict(list)
+        for territory, zone in self._station_statuses.items():
+            zone_to_territories[zone].append(territory)
 
         return {
-            claimant: territories_owned * POINTS_PER_TERRITORY
-            for claimant, territories_owned in territory_counts.items()
-            if claimant != Claimant.UNCLAIMED
+            zone: sum(
+                EXTRA_VALUE_TERRITORIES.get(territory, POINTS_PER_TERRITORY)
+                for territory in zone_to_territories.get(zone, [])
+            )
+            for zone in Claimant.zones()
         }
 
 
