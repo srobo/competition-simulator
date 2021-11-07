b2ec7f8d6bf47298b749819a8fea63a77da6a5b0
WillB97 <will@rudi.org.uk>
2021-11-07 14:52:11 +0000

    Update keyboard robot

--- controllers/example_controller/keyboard_controller.py
+++ controllers/example_controller/keyboard_controller.py
@@ -11,10 +11,10 @@ CONTROLS = {
     "reverse": (ord("S"), ord("K")),
     "left": (ord("A"), ord("J")),
     "right": (ord("D"), ord("L")),
-    "claim": (ord("E"), ord("O")),
     "sense": (ord("Q"), ord("U")),
-    "scan": (ord("R"), ord("P")),
     "boost": (Keyboard.SHIFT, Keyboard.CONTROL),
+    "grabber_open": (ord("R"), ord("P")),
+    "grabber_close": (ord("E"), ord("O")),
 }
 
 
@@ -24,11 +24,10 @@ def print_sensors(robot: Robot) -> None:
         "Front Right",
         "Left",
         "Right",
-        "Back Left",
-        "Back Right",
+        "Front",
+        "Back",
     ]
     touch_sensor_names = [
-        "Front",
         "Rear",
     ]
 
@@ -50,16 +49,14 @@ R = Robot()
 keyboard = Keyboard()
 keyboard.enable(KEYBOARD_SAMPLING_PERIOD)
 
-pending_claims = []
-
 key_forward = CONTROLS["forward"][R.zone]
 key_reverse = CONTROLS["reverse"][R.zone]
 key_left = CONTROLS["left"][R.zone]
 key_right = CONTROLS["right"][R.zone]
-key_claim = CONTROLS["claim"][R.zone]
 key_sense = CONTROLS["sense"][R.zone]
-key_scan = CONTROLS["scan"][R.zone]
 key_boost = CONTROLS["boost"][R.zone]
+key_grab_open = CONTROLS["grabber_open"][R.zone]
+key_grab_close = CONTROLS["grabber_close"][R.zone]
 
 print(
     "Note: you need to click on 3D viewport for keyboard events to be picked "
@@ -71,8 +68,8 @@ while True:
 
     boost = False
 
-    left_power = 0
-    right_power = 0
+    left_power = 0.0
+    right_power = 0.0
 
     while key != NO_KEY_PRESSED:
         key_ascii = key & 0x7F  # mask out modifier keys
@@ -98,21 +95,14 @@ while True:
             left_power += 25
             right_power -= 25
 
-        elif key_ascii == key_claim:
-            R.radio.begin_territory_claim()
-            pending_claims.append(R.time() + 2)
-
         elif key_ascii == key_sense:
             print_sensors(R)
 
-        elif key_ascii == key_scan:
-            transmitters = R.radio.sweep()
-            print("Found transmitter(s)")
+        elif key_ascii == key_grab_open:
+            R.servo_board.servos[0].position = -1
 
-            for transmitter in transmitters:
-                print(transmitter)
-
-            print()
+        elif key_ascii == key_grab_close:
+            R.servo_board.servos[0].position = 1
 
         # Work our way through all the enqueued key presses before dropping
         # out to the timestep
@@ -123,14 +113,7 @@ while True:
         left_power = max(min(left_power * 2, 100), -100)
         right_power = max(min(right_power * 2, 100), -100)
 
-    R.motors[0].m0.power = left_power
-    R.motors[0].m1.power = right_power
-
-    current_time = R.time()
-    # complete claims after their 2 seconds has lapsed
-    for claim_end_time in pending_claims:
-        if current_time > claim_end_time:
-            R.radio.complete_territory_claim()
-            pending_claims.remove(claim_end_time)
+    R.motor_boards[0].motors[0].power = left_power
+    R.motor_boards[0].motors[1].power = right_power
 
     R.sleep(KEYBOARD_SAMPLING_PERIOD / 1000)
