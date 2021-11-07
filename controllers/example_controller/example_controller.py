48a44e8fc9941656891ae2ca72c20682ccfe4cc3
WillB97 <will@rudi.org.uk>
2021-11-07 14:42:11 +0000

    Update example robot code

--- controllers/example_controller/example_controller.py
+++ controllers/example_controller/example_controller.py
@@ -2,46 +2,51 @@ from sr.robot import *
 
 R = Robot()
 
-print("I found {} transmitter(s):".format(len(R.radio.sweep())))
+distance = R.ruggeduino.pins[A5].analogue_read()
+print(f"Rear ultrasound distance: {distance} meters")
 
-# motor board 0, channel 0 to half power forward
-R.motors[0].m0.power = 50
+# first motor board, channel 0 to half power forward
+R.motor_board.motors[0].power = 0.5
 
-# motor board 0, channel 1 to half power forward
-R.motors[0].m1.power = 50
+# motor board "srABC1", channel 1 to half power forward
+# using the syntax to access multiple motor boards
+R.motor_boards['srABC1'].motors[1].power = 0.5
 
 # sleep for 1 second
 R.sleep(1)
 
-# motor board 0, channel 0 to stopped
-R.motors[0].m0.power = 0
+# first motor board, channel 0 to stopped
+R.motor_board.motors[0].power = 0
 
-# motor board 0, channel 1 to stopped
-R.motors[0].m1.power = 0
+# first motor board, channel 1 to stopped
+R.motor_board.motors[1].power = 0
 
 # sleep for 2 seconds
 R.sleep(2)
 
-# motor board 0, channel 0 to half power backward
-R.motors[0].m0.power = -50
+# Turn on the green LED, connected to pin 4
+R.ruggeduino.pins[4].digital_write(True)
 
-# motor board 0, channel 1 to half power forward
-R.motors[0].m1.power = 50
+# first motor board, channel 0 to half power backward
+R.motor_board.motors[0].power = -0.5
+
+# first motor board, channel 1 to half power forward
+R.motor_board.motors[1].power = 0.5
 
 # sleep for 0.75 seconds
-R.sleep(0.75)
+R.sleep(0.25)
 
-# motor board 0, channel 0 to half power forward
-R.motors[0].m0.power = 50
+# first motor board, channel 0 to half power forward
+R.motor_board.motors[0].power = 0.5
 
-# motor board 0, channel 1 to half power forward
-R.motors[0].m1.power = 50
+# first motor board, channel 1 to half power forward
+R.motor_board.motors[1].power = 0.5
 
 # sleep for 1 second
 R.sleep(1)
 
-# motor board 0, channel 0 to stopped
-R.motors[0].m0.power = 0
+# first motor board, channel 0 to stopped
+R.motor_board.motors[0].power = 0
 
-# motor board 0, channel 1 to stopped
-R.motors[0].m1.power = 0
+# first motor board, channel 1 to stopped
+R.motor_board.motors[1].power = 0
