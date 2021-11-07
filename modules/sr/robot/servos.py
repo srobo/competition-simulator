9f5e55c3bc312cc842cbbe7d467e2d80072070e3
WillB97 <will@rudi.org.uk>
2021-11-07 10:34:47 +0000

    Add sr-robot3 servo board

---
+++ modules/sr/robot/servos.py
@@ -0,0 +1,49 @@
+from typing import Dict, List, Union
+
+from controller import Motor, Robot
+from sr.robot.utils import map_to_range, get_robot_device
+
+
+def init_servo_board(webot: Robot) -> 'Dict[str, ServoBoard]':
+    return {
+        'srXYZ1': ServoBoard([Servo(webot, 'test servo')]),
+    }
+
+
+class ServoBoard:
+    def __init__(self, servos: 'List[Servo]') -> None:
+        self.servos = servos
+
+
+class Servo:
+    def __init__(self, webot: Robot, servo_name: str) -> None:
+        self._servo = ServoDevice(webot, servo_name)
+        self._position = 0.0
+
+    @property
+    def position(self) -> float:
+        return self._position
+
+    @position.setter
+    def set_position(self, value: Union[None, float]) -> None:
+        if value is not None:
+            self._position = value
+            self._servo.set_position(value)
+
+
+class ServoDevice:
+    def __init__(self, webot: Robot, servo_name: str) -> None:
+        self.servo_name = servo_name
+        self.webot_motor = get_robot_device(webot, servo_name, Motor)
+        self.max_speed = self.webot_motor.getMaxVelocity()
+        self.max_position = self.webot_motor.getMaxPosition()
+        self.min_position = self.webot_motor.getMinPosition()
+
+    def set_position(self, position: float) -> None:
+        self.webot_motor.setPosition(map_to_range(
+            -1,
+            1,
+            self.min_position,
+            self.max_position,
+            position,
+        ))
