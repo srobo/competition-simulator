2fe31649ecd26a71032ad901c7be4c1d7cb5a4ee
WillB97 <will@rudi.org.uk>
2021-11-12 23:26:07 +0000

    Remove example code used in the microgames

--- controllers/example_controller/example_controller.py
+++ controllers/example_controller/example_controller.py
@@ -12,38 +12,8 @@ R.motor_board.motors[0].power = 0.5
 # using the syntax to access multiple motor boards
 R.motor_boards['srABC1'].motors[1].power = 0.5
 
-# sleep for 1 second
-R.sleep(1)
-
-# first motor board, channel 0 to stopped
-R.motor_board.motors[0].power = 0
-
-# first motor board, channel 1 to stopped
-R.motor_board.motors[1].power = 0
-
-# sleep for 2 seconds
+# sleep for 2 second
 R.sleep(2)
-
-# Turn on the green LED, connected to pin 4
-R.ruggeduino.pins[4].digital_write(True)
-
-# first motor board, channel 0 to half power backward
-R.motor_board.motors[0].power = -0.5
-
-# first motor board, channel 1 to half power forward
-R.motor_board.motors[1].power = 0.5
-
-# sleep for 0.75 seconds
-R.sleep(0.25)
-
-# first motor board, channel 0 to half power forward
-R.motor_board.motors[0].power = 0.5
-
-# first motor board, channel 1 to half power forward
-R.motor_board.motors[1].power = 0.5
-
-# sleep for 1 second
-R.sleep(1)
 
 # first motor board, channel 0 to stopped
 R.motor_board.motors[0].power = 0
