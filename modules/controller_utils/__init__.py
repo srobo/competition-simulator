144249e08bc43a0b3836df30c3ddfa96008d53f0
Peter Law <PeterJCLaw@gmail.com>
2023-01-14 16:43:47 +0000

    Update the number of zones and match duration for SR2023

--- modules/controller_utils/__init__.py
+++ modules/controller_utils/__init__.py
@@ -24,9 +24,9 @@ if not ARENA_ROOT.is_absolute():
     raise ValueError(f"'ARENA_ROOT' must be an absolute path, got {ARENA_ROOT!r}")
 
 
-NUM_ZONES = 1
+NUM_ZONES = 4
 
-GAME_DURATION_SECONDS = 120
+GAME_DURATION_SECONDS = 150
 
 
 class Resolution(NamedTuple):
