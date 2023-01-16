1b60dbe426f60e0c7b9fd70a7b794953549b699f
WillB97 <will@rudi.org.uk>
2023-01-16 22:55:50 +0000

    Vendor in squaternion

---
+++ modules/squaternion/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2014 Kevin Walchko
+# see LICENSE for full details
+##############################################
+from squaternion.squaternion import Quaternion
+
+from importlib.metadata import version # type: ignore
+
+__author__ = "Kevin Walchko"
+__license__ = "MIT"
+__version__ = version("squaternion")
+
+__all__ = [
+    'Quaternion',
+]
