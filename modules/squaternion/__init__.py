3de3e0d838e5051057316cc22d60a909c767c450
WillB97 <will@rudi.org.uk>
2023-01-21 17:25:14 +0000

    Fix squaternion finding it's package version

--- modules/squaternion/__init__.py
+++ modules/squaternion/__init__.py
@@ -4,13 +4,11 @@
 # Copyright (c) 2014 Kevin Walchko
 # see LICENSE for full details
 ##############################################
-from importlib.metadata import version
-
 from squaternion.squaternion import Quaternion
 
 __author__ = "Kevin Walchko"
 __license__ = "MIT"
-__version__ = version("squaternion")
+__version__ = "0.3.3"
 
 __all__ = [
     'Quaternion',
