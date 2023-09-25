ce8d0eec7294e689b4e44dfcda9229ccb60fed82
WillB97 <will@rudi.org.uk>
2023-09-25 21:46:58 +0100

    Convert the example robot's output to meters

--- controllers/example_controller/example_controller.py
+++ controllers/example_controller/example_controller.py
@@ -3,7 +3,9 @@ from sr.robot3 import *
 R = Robot()
 
 reading = R.arduino.pins[A5].analog_read()
-print(f"Rear ultrasound distance value: {reading}")
+# convert reading from volts to meters
+measurement = reading / 2.5
+print(f"Rear ultrasound distance {measurement:.2f} meters")
 
 # first motor board, channel 0 to half power forward
 R.motor_board.motors[0].power = 0.5
