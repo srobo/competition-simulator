4745f2cd3e2e7a32ef1586a9552b90ca90dbb66e
Peter Law <PeterJCLaw@gmail.com>
2023-10-17 20:24:58 +0100

    Update example controllers to use canonical example variable name
    Fixes https://github.com/srobo/competition-simulator/issues/436

--- controllers/example_controller/example_controller.py
+++ controllers/example_controller/example_controller.py
@@ -1,22 +1,22 @@
 from sr.robot3 import *
 
-R = Robot()
+robot = Robot()
 
-reading = R.arduino.pins[A5].analog_read()
+reading = robot.arduino.pins[A5].analog_read()
 print(f"Rear ultrasound distance value: {reading}")
 
 # first motor board, channel 0 to half power forward
-R.motor_board.motors[0].power = 0.5
+robot.motor_board.motors[0].power = 0.5
 
 # motor board "srABC1", channel 1 to half power forward
 # using the syntax to access multiple motor boards
-R.motor_boards['srABC1'].motors[1].power = 0.5
+robot.motor_boards['srABC1'].motors[1].power = 0.5
 
 # sleep for 2 second
-R.sleep(2)
+robot.sleep(2)
 
 # first motor board, channel 0 to stopped
-R.motor_board.motors[0].power = 0
+robot.motor_board.motors[0].power = 0
 
 # first motor board, channel 1 to stopped
-R.motor_board.motors[1].power = 0
+robot.motor_board.motors[1].power = 0
